8fb44bc2617fa56840e656177497e1adbfd2dd1c
Russel Neiss <russel.neiss@gmail.com>
2022-10-07 10:10:48 -0500

    (feat): debounce geocode api call, add in stub for grabbing POI data

--- main.js
+++ main.js
@@ -20,10 +20,24 @@ const map = new Map({
 });
 
 async function geoCode(lat, lon) {
-    await fetch(`https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${lat}&lon=${lon}&zoom=14&accept-language=en`)
+
+    const options = {
+        method: 'GET',
+        headers: {
+        }
+    };
+
+    await fetch(`https://forward-reverse-geocoding.p.rapidapi.com/v1/reverse?lat=${lat}&lon=${lon}&accept-language=he&polygon_threshold=0.0`, options)
         .then((r) => r.json()).then( data => {
-            const isWb = data['features'][0]['properties']['address']['country_code'] == "ps"
-            const placeArray = data['features'][0]['properties']['display_name'].split(',')
+            console.log(data)
+            const isWb = data['address']['country_code'] == "ps"
+            let placeArray = []
+            if (data['address']['suburb']) placeArray.push(data['address']['suburb'])
+            if (data['address']['neighbourhood']) placeArray.push(data['address']['neighbourhood'])
+            if (data['address']['village']) placeArray.push(data['address']['village'])
+            if (data['address']['city']) placeArray.push(data['address']['city'])
+            if (data['address']['state_district']) placeArray.push(data['address']['state_district'])
+            // const placeArray = data['display_name'].split(',')
             getNLI(placeArray, isWb)
       })
 }
@@ -32,10 +46,94 @@ function generateNliKeyword(place, isWb) {
     place = place.replace("Regional Council", "")
         .replace("Subdistrict", "")
         .replace("District", "")
+        .replace("נפת", "")
+        .replace("מחוז", "")
+
     return `${place}`.trim()
 
     // return `${place}*(${isWb ? "West Bank": "Israel"})*`.trim()
 }
+
+
+function getBoundingBox(centerPoint, distance) {
+    //distance is in km
+    let MIN_LAT, MAX_LAT, MIN_LON, MAX_LON, R, radDist, degLat, degLon, radLat, radLon, minLat, maxLat, minLon, maxLon, deltaLon;
+    if (distance < 0) {
+        return 'Illegal arguments';
+    }
+    // helper functions (degrees<–>radians)
+    Number.prototype.degToRad = function () {
+        return this * (Math.PI / 180);
+    };
+    Number.prototype.radToDeg = function () {
+        return (180 * this) / Math.PI;
+    };
+    // coordinate limits
+    MIN_LAT = (-90).degToRad();
+    MAX_LAT = (90).degToRad();
+    MIN_LON = (-180).degToRad();
+    MAX_LON = (180).degToRad();
+    // Earth's radius (km)
+    R = 6378.1;
+    // angular distance in radians on a great circle
+    radDist = distance / R;
+    // center point coordinates (deg)
+    degLat = centerPoint[0];
+    degLon = centerPoint[1];
+    // center point coordinates (rad)
+    radLat = degLat.degToRad();
+    radLon = degLon.degToRad();
+    // minimum and maximum latitudes for given distance
+    minLat = radLat - radDist;
+    maxLat = radLat + radDist;
+    // minimum and maximum longitudes for given distance
+    minLon = void 0;
+    maxLon = void 0;
+    // define deltaLon to help determine min and max longitudes
+    deltaLon = Math.asin(Math.sin(radDist) / Math.cos(radLat));
+    if (minLat > MIN_LAT && maxLat < MAX_LAT) {
+        minLon = radLon - deltaLon;
+        maxLon = radLon + deltaLon;
+        if (minLon < MIN_LON) {
+            minLon = minLon + 2 * Math.PI;
+        }
+        if (maxLon > MAX_LON) {
+            maxLon = maxLon - 2 * Math.PI;
+        }
+    }
+    // a pole is within the given distance
+    else {
+        minLat = Math.max(minLat, MIN_LAT);
+        maxLat = Math.min(maxLat, MAX_LAT);
+        minLon = MIN_LON;
+        maxLon = MAX_LON;
+    }
+    return [
+        minLon.radToDeg(),
+        minLat.radToDeg(),
+        maxLon.radToDeg(),
+        maxLat.radToDeg()
+    ];
+};
+
+
+async function getPOIs(mapCenter) {
+    const bbox = getBoundingBox(mapCenter, 2)
+    const query = encodeURI(`data=[out:json][timeout:2];(node["historic"](${bbox[0]},${bbox[1]},${bbox[2]},${bbox[3]});way["historic"](${bbox[0]},${bbox[1]},${bbox[2]},${bbox[3]});++relation["historic"](${bbox[0]},${bbox[1]},${bbox[2]},${bbox[3]}););out+body;>;out+skel+qt;`)
+
+    await fetch("https://overpass-api.de/api/interpreter", {
+        "body": query,
+        "method": "POST",
+    }).then((r) => r.json()).then( data => {
+        console.log(data)
+
+        data.elements.forEach(poi => {
+            console.log(poi.tags.name)
+        })
+    })
+
+}
+
 
 async function getNLI(placeArray, isWb) {
     document.querySelector("#nli_images").innerHTML = "Loading..."
@@ -78,10 +176,16 @@ async function getNLI(placeArray, isWb) {
 
                 records.forEach(record => {
                     const tag907 = record.querySelector("[*|tag='907']")
+                    const tag245 = record.querySelector("[*|tag='245']")
+                    let titleString = ""
+                    if (tag245) {titleString = titleString + `${tag245.querySelector("[*|code='a']").innerHTML} `}
                     if (tag907) {
                         const image = document.createElement('img')
                         image.className = "nliImage"
                         image.src  = `https://rosetta.nli.org.il/delivery/DeliveryManagerServlet?dps_func=stream&dps_pid=${tag907.querySelector("[*|code='d']").innerHTML}`
+
+                        image.title = titleString
+
                         document.querySelector('#nli_images').appendChild(image);
                     }
                 });
@@ -91,29 +195,25 @@ async function getNLI(placeArray, isWb) {
 
 }
 
+let debounce
 function onMoveStart(evt) {
+    clearTimeout(debounce);
     stopSearch = true
 }
 
 function onMoveEnd(evt) {
-  stopSearch = false
-  const map = evt.map;
-  const latLon = transform(map.getView().getCenter(),'EPSG:3857', 'EPSG:4326')
-  geoCode(latLon[1], latLon[0])
-  console.log(
-      transform(
-        map.getView().getCenter(),
-        'EPSG:3857', 'EPSG:4326'
-      )
+    stopSearch = false
+    const map = evt.map;
+    const latLon = transform(map.getView().getCenter(), 'EPSG:3857', 'EPSG:4326')
 
-      )
-  // const extent = map.getView().calculateExtent(map.getSize());
-  // const bottomLeft = toLonLat(getBottomLeft(extent));
-  // const topRight = toLonLat(getTopRight(extent));
-  // display('left', wrapLon(bottomLeft[0]));
-  // display('bottom', bottomLeft[1]);
-  // display('right', wrapLon(topRight[0]));
-  // display('top', topRight[1]);
+    debounce = setTimeout(
+
+    function () {
+        geoCode(latLon[1], latLon[0])
+    }, 1000);
+    // getPOIs(latLon)
+    console.log(latLon)
+    console.log(getBoundingBox(latLon, 2))
 }
 
 map.on('moveend', onMoveEnd);
